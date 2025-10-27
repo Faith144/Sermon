914213ad86aebd3560281b4bbbe8b3d67dab1217
Bayodele Faith <bayodele144@gmail.com>
2025-10-27 05:08:44 +0100

    added telegram audio

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from .models import Note, Sermon, Preacher
+from .models import Note, Sermon, Preacher, Telegram_audio
+from django.contrib.auth.models import Group
 
 # Register your models here.
+admin.AdminSite.site_header = "Sermon Administration"
+admin.AdminSite.site_title = "Sermon Admin Portal"
+admin.AdminSite.index_title = "Welcome to the Sermon Admin Portal"
+admin.AdminSite.unregister(Group)
+
 admin.site.register(Note)
 admin.site.register(Sermon)
 admin.site.register(Preacher)
+admin.site.register(Telegram_audio)
