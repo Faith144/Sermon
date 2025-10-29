b2a0661922888e943be5fedc7c27e5ae959c6271
Bayodele Faith <153163690+Faith144@users.noreply.github.com>
2025-10-29 04:11:23 +0100

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = config(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG")
+DEBUG = False
 
 ALLOWED_HOSTS = config(
     "ALLOWED_HOSTS",
